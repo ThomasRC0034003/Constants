34424a1aae3589666b1154c64f0482eb8f710660
rc0034003 <rc0034003@CRO-24-18>
2019-02-26 09:31:39 +0000

    kjbk

--- Constants/Form1.cs
+++ Constants/Form1.cs
@@ -38,7 +38,18 @@ namespace Constants
                 MessageBox.Show(s2 + " is not an integer");
                 int i2 = 0;
             }
+            
 
+            string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+            catch
+            {
+                double d2 = 0;
+                richTextBox1.AppendText(d2.ToString() + "\n");
+            }
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
